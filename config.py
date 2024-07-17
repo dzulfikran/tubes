a197db5f5bdb6b37852f46a11a9512396157bd30
dzulfikran <107086124+dzulfikran@users.noreply.github.com>
2024-07-18 06:25:31 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/notisq'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'static/images'
-    FOLDER_PROFILE_P = 'static/profile-pic'
